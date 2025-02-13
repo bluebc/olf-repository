0556b4d49919b35d97ecbbd6b752b2266aca5890
unknown <shkim@bluebc.com>
2025-02-13 17:11:12 +0900

    test: before merge2

--- MergeTest.java
+++ MergeTest.java
@@ -1,13 +1,13 @@
 public Class MergeTest{
 	
 	public void test1{
-		String a1 = "a1";
+		String a1 = "a2";
 	}
 	
 	public String test2{
 		String result = "t2";
 		
-		
+		result += "added";
 		
 		
 		
