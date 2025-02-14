85ad76de42207820aea9137aef801d93126b72a5
JY00088 <dobisms@gmai.com>
2025-02-14 14:49:34 +0900

    스와이프js수정

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,27 @@
 
-var swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
+// var swiper = new Swiper(".swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
 
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
+
+// 헤더 슬라이드
+$(document).ready(function(){
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  })
 });
