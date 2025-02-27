cfcd7d20eea4893a2beff8369b7d2c431792482c
unknown <jhlee@bluebc.com>
2025-02-27 14:43:25 +0900

    uallfile

---
+++ js/menu_scroll.js
@@ -0,0 +1,10 @@
+//메뉴 클릭시 자연스러운 스크롤 이동
+
+$(document).ready(function($) {
+    $(".scroll").click(function(){
+    /* event.preventDefault(); */
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+    });
+    });
+
+
